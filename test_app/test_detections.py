99e910bc3199a9f3234b87a931801a7d6c2569de
NebojsaJovanovic95 <nebojsajovanovic@gmail.com>
2025-11-30 18:06:54 -0500

    quicksave

--- test_app/test_detections.py
+++ test_app/test_detections.py
@@ -4,9 +4,7 @@ SERVER_URL = "http://yolov8_server:5000"
 TEST_IMAGE = "test.jpg"
 
 def run_test():
-    with open(TEST_IMAGE, "rb") as f:
-        files = {"file": f}
-        response = requests.post(f"{SERVER_URL}/detect", files=files)
+    response = requests.get(f"{SERVER_URL}/detections")
     
     print("Status code:", response.status_code)
     print("Response:", response.json())
--- test_app/test_post.py
+++ test_app/test_post.py
@@ -4,14 +4,14 @@ import test_detections
 import test_stream
 
 def main():
-    print("Runnning /detect test...")
-    id = test_detection.run_test()
-    print(f"Runnning /detection/{id} test...")
-    test_detection_id.run_test(id=id)
+#    print("Runnning /detect test...")
+#    id = test_detection.run_test()
+#    print(f"Runnning /detection/{id} test...")
+#    test_detection_id.run_test(id=id)
     print("Runnning /detections test...")
     test_detections.run_test()
-    print("Runnning /stream test...")
-    test_stream.run_test()
+#    print("Runnning /stream test...")
+#    test_stream.run_test()
 
 if __name__ == "__main__":
     main()
